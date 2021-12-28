40a5677ffbeef0648f10848f1ec44bca81e0d144
Millicent Pelamonia <millicentpelamonia@MBPvanMillicent.home>
2021-12-28 15:25:26 +0100

    Menu changes

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,32 +13,31 @@
     <q-header elevated>
       <q-toolbar>
         <q-btn
+          round
           flat
           dense
-          round
           icon="menu"
           aria-label="Menu"
           @click="toggleLeftDrawer"
         />
 
         <q-toolbar-title>
-          Quasar App
+          Restaurant App
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div>Restaurant app V{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
     <q-drawer
       v-model="leftDrawerOpen"
-      show-if-above
       bordered
     >
       <q-list>
         <q-item-label
           header
         >
-          Essential Links
+          Menu
         </q-item-label>
 
         <EssentialLink
@@ -64,47 +63,31 @@ import AppError from 'components/AppError.vue'
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev',
+    title: 'Home',
+    icon: 'home',
+    link: '/#/',
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework',
+    title: 'About',
+    icon: 'info',
+    link: '/#/about',
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev',
+    title: 'All restaurants',
+    icon: 'restaurant',
+    link: '/#/restaurants',
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev',
+    title: 'My profile',
+    icon: 'account_circle',
+    link: '/#/myprofile',
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
+    title: 'Settings',
+    icon: 'settings',
+    link: '/#/settings',
   },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
-  },
+
 ];
 
 import { defineComponent, ref } from 'vue';
